071d948e3ad9b1b7842f93976bd13c054ebeb412
LARRY MOSQUERA <larrysalsa@icloud.com>
2023-06-07 22:38:54 -0500

    Agregar archivos de proyecto.

---
+++ Metodos/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO SOY UN PROGRAMADOR  EN C# Y EXPERTO EN REDES EXCELENTE");
+            //variables
+            int opcion;
+            decimal r; //almacena el valor devuelto de restar
+            decimal num1Ar, num2Ar; //Argumentos para enviar una copia de su valor a los metodos
+
+            //mostramos el menu
+
+
+            do
+            {
+                Console.WriteLine("1.  Suma");
+                Console.WriteLine("2.  Resta");
+                Console.WriteLine("3. Multiplicacion");
+                Console.WriteLine("4. Division");
+
+
+                Console.Write("Escoge una opcion:  ");
+                opcion = Convert.ToByte(Console.ReadLine());
+            }
+            while ((opcion < 1) || (opcion > 4));
+
+
+
+
+            switch (opcion)
+            {
+                case 1:
+                    Sumar();
+                    break;
+
+
+                case 2:
+                    r = Resta(); //asignamos a "r" el valor devuelto con return
+
+                    //Mostramos el resultado con la informacion que contiene "r"
+                    Console.WriteLine("El resultado de la resta es:  {0}", r);
+
+
+                    //mostramos el resultado con la informacion que tiene "r"
+                    break;
+
+                case 3:
+                    //Pedimos ambos numeros
+                    Console.Write("Ingresa el primer numero :  ");
+                    num1Ar = Convert.ToDecimal(Console.ReadLine());
+
+                    Console.Write("Ingresa el segundo numero :   ");
+                    num2Ar = Convert.ToDecimal(Console.ReadLine());
+                    //Invocamos al metodo
+                    Multiplicacion(num1Ar, num2Ar);
+                    break;
+
+                case 4:
+                    //Pedimos ambos numeros
+                    Console.Write("Ingresa el primer numero:  ");
+                    num1Ar = Convert.ToDecimal(Console.ReadLine());
+                    Console.Write("Ingrese el segund numero:  ");
+                    num2Ar = Convert.ToDecimal(Console.ReadLine());
+
+                    //invocamos el metodo
+                    r = Division(num1Ar, num2Ar);
+
+                    //Mostramos el resultado con la informacion que contiene "r"
+                    Console.WriteLine("El resultado de la division es:  {0}", r);
+                    break;
+
+
+
+
+            }
+
+        }//cierre del main
+        // [modificador] [tipo] [identificador] [parametros]
+        static void Sumar()
+        {
+            decimal num1, num2, resultado;
+            //pedimos el valor de ambos numeros
+            Console.Write("Ingresa el primer numero:");
+            num1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Ingresa el segundo numero:");
+            num2 = Convert.ToDecimal(Console.ReadLine());
+
+            //operacion 
+            resultado = num1 + num2;
+
+            Console.WriteLine("{0}+{1}= {2}", num1, num2, resultado);
+
+
+        }
+
+        static decimal Resta()
+        {
+            //variables del metodo restar
+            decimal num1, num2, resultado;
+
+            //pedimos ambos numeros
+            Console.Write("Ingresa el primer numero :  ");
+            num1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo numero: ");
+            num2 = Convert.ToDecimal(Console.ReadLine());
+
+            //Operacion
+            resultado = num1 - num2;
+            //devolvemos un tipo al autor del llamado
+            return resultado;
+
+        }
+
+        // [modificador] {tipo] [identificador] [parametros]
+        static void Multiplicacion(decimal num1Pa, decimal num2Pa)
+        {
+
+            //Variables
+            decimal resultado;
+
+            //Multiplicacion con los valores que mandaron los argumentos
+            resultado = num1Pa * num2Pa;
+            //Mostramos el resultado
+            Console.WriteLine("{0}*{1}={2}", num1Pa, num2Pa, resultado);
+        }
+        // [modificador] {tipo] [identificador] [parametros]
+        static decimal Division(decimal num1Pa, decimal num2Pa)
+        {
+            //Variables
+
+            decimal resultado;
+
+            if (num2Pa != 0)
+            {
+
+                resultado = num1Pa / num2Pa;
+            }
+            else
+            {
+                Console.WriteLine("No es posible dividir entre cero");
+                resultado = 0;
+
+            }
+
+            return resultado;
+            //division cion con los valores que mandaron los argumentos
+            //Resultado
+
+
+
+            resultado = num1Pa / num2Pa;
+            Console.WriteLine("{0}/{1}={2}", num1Pa, num2Pa, resultado);
+
+        }
+
+
+    }
+}
